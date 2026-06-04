4fda0126e77ba5f156d40c2e60cab5622e89e6bf
Afonso Nzango <afonso1.nzango@gmail.com>
2026-06-04 10:58:59 +0100

    Fix double message & set OTP to 6 caracters

--- legacy/gerar_hash_novo.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo "<h3>Diretor</h3>";
-echo password_hash("diretor1", PASSWORD_ARGON2ID);
-
-echo "<hr>";
-
-echo "<h3>Secretaria</h3>";
-echo password_hash("secretaria12", PASSWORD_ARGON2ID);
-?>
